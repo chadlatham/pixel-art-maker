f164ddf465626d23cf223187eb279579942c4b78
Chad Latham <chadlatham33@gmail.com>
2016-05-14 15:06:02 -0700

    Updating assignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,48 @@
-// The global object to wrap global variables and function declarations.
+'use strict';
+// The global object to wrap global variables and function declarations
+// Avoids poluting the global namespace
 var app = {};
+
+
+// Generates a nested div structure with no text nodes between inner div elements.
+// rows and columns must be even numbers.
+app.generateGrid = function(cellCount) {
+  var grid = document.querySelector('.grid');
+
+  // Clear the existing children in grid
+  var toClear = [];
+  for (var i = 0; i < grid.childNodes.length; i++) {
+    toClear.push(grid.childNodes[i]);
+  }
+  for (var i = 0; i < toClear.length; i++) {
+    grid.removeChild(toClear[i]);
+    // console.log(grid.childNodes.length);
+  }
+  console.log(grid.parentElement);
+
+  // Loop and create the cell instances
+  for (var n = 0; n < cellCount; n++) {
+    var cell = document.createElement('div');
+    cell.className = 'cell';
+    grid.appendChild(cell);
+  }
+  // console.log(window.innerHeight);
+  // console.log(grid.childNodes.length);
+  console.log(grid.parentElement);
+}
+
+
 
 
 
 
 // Keep at bottom of file
 // Start scripting after DOM loads
-var executeScripts = function (event) {
+app.executeScripts = function (event) {
   console.log('DOMContentLoaded');
+  // document.getElementsByClassName('row')[0].childNodes[2].remove();
+  // console.log(document.getElementsByClassName('row'));
+  app.generateGrid(2006);
 }
 
-document.addEventListener('DOMContentLoaded', executeScripts);
+document.addEventListener('DOMContentLoaded', app.executeScripts);
